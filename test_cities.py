4081c1bd274c2c86b74bb14da6eb34ec5f194c2b
mechikuroki <kurokimercedes@gmail.com>
2026-03-18 08:22:19 -0300

    me había olvidado de poner un test que falle

--- test_cities.py
+++ test_cities.py
@@ -1,5 +1,11 @@
 from city_functions import ciudadpais
 
-def test_ciudad_pais():
+def test_ciudad_pais_pasa():
     string = ciudadpais("Buenos Aires", "Argentina")
     assert string == "Buenos Aires, Argentina"
+
+def test_ciudad_pais_falla():
+    string = ciudadpais("Gerli", "Buenos Aires", "Argentina")
+    assert string == "Gerli, Buenos Aires, Argentina"
+
+
